08f9de110c4965d6b95c7f590f19cc791c11af3f
Zelin Zhang <ZZhang@indigo.ca>
2018-08-24 10:59:37 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// /**
+//  * Scrape job title, url, and location from Taleo jobs page at https://l3com.taleo.net/careersection/l3_ext_us/jobsearch.ftl
+//  *
+//  * Usage: $ casperjs scraper.js 
+//  */
+// var casper = require("casper").create({
+//     pageSettings: {
+//         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+//     }
+// });
+
+// var url = 'https://www.walmart.ca/search/6000192385877';
+// //var currentPage = 1;
+// var jobs = [];
+
+// var terminate = function() {
+//     this.echo("Exiting..").exit();
+// };
+
+// // Return the current page by looking for the disabled page number link in the pager
+// // function getSelectedPage() {
+// //     var el = document.querySelector('li[class="navigation-link-disabled"]');
+// //     return parseInt(el.textContent);
+// // }
+
+// function getJobs() {
+//     console.log("TriHard 7");
+//     var rows = document.querySelectorAll('div');
+//     console.log(rows);
+//     console.log(rows.length);
+//     var jobs = [];
+//     console.log("get jobs");
+//     //var a = row.cells[1].querySelector('a[href*="jobdetail.ftl?job="]');
+//     //var l = row.cells[2].querySelector('span');
+//     //var job = {};
+
+//     //job['link'] = rows.getAttribute('href');
+//     //job['url'] = a.getAttribute('href');
+//     //job['location'] = l.innerText;
+//     jobs.push(rows.getAttribute('href')); 
+//     console.log("push jobs");
+//     return jobs;       
+// }
+
+// var processPage = function() {
+//     console.log("starto 2");
+//     jobs = casper.evaluate(getJobs);
+//     console.log(jobs);
+//     require('utils').dump(jobs);
+
+//     // this.thenClick("div#jobPager a#next").then(function() {
+//     //     this.waitFor(function() {
+//     //         return currentPage === this.evaluate(getSelectedPage);
+//     //     }, processPage, terminate);
+//     // });
+    
+//     return terminate.call(casper);
+// };
+
+// casper.start(url);
+// console.log("starto");
+
+// casper.waitForSelector('article > a[class="product-link"]', processPage, terminate);
+// casper.run();
+
+var links = [];
+var casper = require('casper').create();
+
+function getLinks() {
+    var links = document.querySelectorAll('a.product-link');
+    return Array.prototype.map.call(links, function(e) {
+        return e.getAttribute('href');
+    });
+}
+
+casper.start('https://www.walmart.ca/search/673419233606', function() {
+   // Wait for the page to be loaded
+   this.waitForSelector('a[class="product-link"]');
+});
+
+// casper.then(function() {
+//    // search for 'casperjs' from google form
+//    this.fill('form[action="/search"]', { q: 'casperjs' }, true);
+// });
+
+casper.then(function() {
+    // aggregate results for the 'casperjs' search
+    links = this.evaluate(getLinks);
+    // now search for 'phantomjs' by filling the form again
+    //this.fill('form[action="/search"]', { q: 'phantomjs' }, true);
+});
+
+casper.then(function() {
+    // aggregate results for the 'phantomjs' search
+    links = links.concat(this.evaluate(getLinks));
+});
+
+casper.run(function() {
+    // echo results in some pretty fashion
+    //this.echo(links.length + ' links found:');
+    this.echo(links[0]).exit();
+    //this.echo(' - ' + links.join('\n - ')).exit();
+});
---
+++ main.js
@@ -0,0 +1,62 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var shellescape = require('shell-escape');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => { 
+    var cmd = "casperjs index.js";
+    exec(cmd, {}, function (error, stdout) {
+        if(error) res.status(500).send(error);
+        return res.status(200).send(stdout);
+    });    
+});
+app.post('/', function (req, res) {
+
+    var url = req.body.__url;
+    var action = req.body.__action;
+    var header = req.body.__header;
+    var creds = req.body.__creds;
+    delete req.body.__url;
+    delete req.body.__action;
+    delete req.body.__header;
+    delete req.body.__creds;
+
+    creds = JSON.parse(creds);
+    creds = JSON.stringify(creds);
+
+    var postData = JSON.stringify(req.body);
+
+    var args = [
+        'casperjs',
+        './index.js',
+        '--web-security=no',
+        '--ignore-ssl-errors=true',
+        '--ssl-protocol=any',
+        url,
+        action,
+        creds,
+        postData
+    ];
+
+    var cmd = shellescape(args);
+
+    exec(cmd, function (error, stdout) {
+        if(error) console.log(error);
+        res.end(stdout);
+    });
+
+});
+
+var port_number = process.env.PORT || 3000;
+
+app.listen(port_number, () => console.log(`Listening on ` + port_number));
