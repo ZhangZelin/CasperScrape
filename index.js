cb539f26fca8bfd11b3d19b5afded0a086424592
Zelin Zhang <ZZhang@indigo.ca>
2018-08-27 16:04:04 -0400

    all html

--- index.js
+++ index.js
@@ -66,17 +66,20 @@
 var links = [];
 var casper = require('casper').create();
 
+var html;
+
 var url = casper.cli.raw.get('url');
 var classifyingselector = casper.cli.raw.get('classifying-selector');
 //var selector = casper.cli.get('selector');
 //var attribute = casper.cli.get('attribute');
 
-function getLinks() {
-    var links = document.querySelectorAll('a.product-link');
-    return Array.prototype.map.call(links, function(e) {
-        return e.getAttribute('href');
-    });
-}
+// function getLinks() {
+//     var links = document.querySelectorAll('a.product-link');
+//     return Array.prototype.map.call(links, function(e) {
+//         return e.getAttribute('href');
+//     });
+// }
+
 //'https://www.walmart.ca/search/673419233606'
 casper.start(url, function() {
    // Wait for the page to be loaded
@@ -89,21 +92,25 @@ casper.start(url, function() {
 //    this.fill('form[action="/search"]', { q: 'casperjs' }, true);
 // });
 
-casper.then(function() {
-    // aggregate results for the 'casperjs' search
-    links = this.evaluate(getLinks);
-    // now search for 'phantomjs' by filling the form again
-    //this.fill('form[action="/search"]', { q: 'phantomjs' }, true);
+casper.then(function(){
+    html = document.documentElement.outerHTML;
 });
 
-casper.then(function() {
-    // aggregate results for the 'phantomjs' search
-    links = links.concat(this.evaluate(getLinks));
-});
+// casper.then(function() {
+//     // aggregate results for the 'casperjs' search
+//     links = this.evaluate(getLinks);
+//     // now search for 'phantomjs' by filling the form again
+//     //this.fill('form[action="/search"]', { q: 'phantomjs' }, true);
+// });
+
+// casper.then(function() {
+//     // aggregate results for the 'phantomjs' search
+//     links = links.concat(this.evaluate(getLinks));
+// });
 
 casper.run(function() {
     // echo results in some pretty fashion
     //this.echo(links.length + ' links found:');
-    this.echo(links[0]).exit();
+    this.echo(html).exit();
     //this.echo(' - ' + links.join('\n - ')).exit();
 });
